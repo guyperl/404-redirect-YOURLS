7260f21a73cdf275c43f239a783957f45087479b
1Conan <andremarx13@gmail.com>
2015-06-14 20:34:05 +0800

    Fixed whole Plugin

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ Installation
 1. In `/user/plugins`, create a new folder named `404-redirect`.
 2. Drop these files in that directory.
 3. Go to the Plugins administration page ( *eg* `http://sho.rt/admin/plugins.php` ) and activate the plugin.
+4. Add " define( 'CON_404_PAGE', '/* Path to error page from webserver root*/' ); " to config.php
+Example:
+" define( 'CON_404_PAGE', 'errorpages/404.html' ); "
 4. Have fun!
 
 License
--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: 404-Redirect
 Plugin URI: https://github.com/1Conan/404-redirect-YOURLS
 Description: A simple plugin which sets the 404 header for unkown links.
-Version: 1.0
+Version: 1.1
 Author: 1Conan
 Author URI: https://github.com/1Conan
 */
@@ -11,7 +11,11 @@ Author URI: https://github.com/1Conan
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-yourls_add_action('redirect_keyword_not_found', '1c_setheader_404');
-function 1c_setheader_404() {
+yourls_add_action('redirect_keyword_not_found', 'conan_setheader_404');
+function conan_setheader_404() {
 	http_response_code(404);
+	$_SERVER['REDIRECT_STATUS'] = 404;
+	$handle = curl_init('http://'. $_SERVER["HTTP_HOST"] .'/'.CON_404_PAGE);
+	curl_exec($handle);
+	die();
 }
