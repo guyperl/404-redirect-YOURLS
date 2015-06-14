fece0827b43def07f18a1bf6518bb82411f8bf13
1Conan <andremarx13@gmail.com>
2015-06-14 20:01:31 +0800

    Initial code for plugin

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: 404-Redirect
+Plugin URI: https://github.com/1Conan/404-redirect-YOURLS
+Description: A simple plugin which sets the 404 header for unkown links.
+Version: 1.0
+Author: 1Conan
+Author URI: https://1conan.com/
+*/
+
+// No direct call
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+yourls_add_action('redirect_keyword_not_found', '1c_setheader_404');
+function 1c_setheader_404() {
+	http_response_code(404);
+}
