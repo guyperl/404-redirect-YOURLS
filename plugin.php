6abf64f9ec93620f583d0c8159d29b57e9e0f4ac
1Conan <1Conan@users.noreply.github.com>
2015-12-05 20:17:00 +0900

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 	Plugin Name: 404-Redirect
 	Plugin URI: https://github.com/1Conan/404-redirect-YOURLS
 	Description: A simple plugin which sets the 404 header and shows the 404 page for unknown links.
-	Version: 1.3
+	Version: 1.2
 	Author: 1Conan
 	Author URI: https://github.com/1Conan
 */
@@ -14,13 +14,14 @@ if( !defined( 'YOURLS_ABSPATH' ) ) die();
 yourls_add_action('redirect_keyword_not_found', 'conan_setheader_404');
 function conan_setheader_404() {
 	yourls_status_header( 404 );
+	$a = (isset($_SERVER['HTTPS'])) ? "https://" : "http://";
 	if(function_exists('curl_exec')){
-		$handle = curl_init('http://'. $_SERVER["HTTP_HOST"] .'/'.CON_404_PAGE);
+		$handle = curl_init( $a . $_SERVER["HTTP_HOST"] .'/'.CON_404_PAGE );
 		curl_exec($handle);
 	} else if (ini_get('allow_url_fopen')) {
-		echo file_get_contents('http://'. $_SERVER["HTTP_HOST"] .'/'.CON_404_PAGE);
+		echo file_get_contents( $a . $_SERVER["HTTP_HOST"] .'/'.CON_404_PAGE );
 	} else {
-		yourls_redirect( 'http://'. $_SERVER["HTTP_HOST"] .'/'.CON_404_PAGE, 404 );
+		yourls_redirect( $a . $_SERVER["HTTP_HOST"] .'/'.CON_404_PAGE, 404 );
 	}
 	die();
 }
